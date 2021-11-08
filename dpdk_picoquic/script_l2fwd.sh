b4c89b1f6c9ecabb768ca3975d8c47c7ec263b13
Kikito <nikita.tyunyayev@gmail.com>
2021-11-08 20:49:07 +0100

    adding scripts

---
+++ dpdk_picoquic/script_l2fwd.sh
@@ -0,0 +1,1 @@
+sudo /home/nikita/Documents/memoire/dpdk-20.11.3/dpdk-stable-20.11.3/build/examples/dpdk-l2fwd -- -p 1
---
+++ dpdk_picoquic/script_pktgen.sh
@@ -0,0 +1,1 @@
+sudo /home/nikita/Documents/local/Pktgen-DPDK/usr/local/bin/pktgen -n 4 -- -P -p 0x1 -m "1.0" 
