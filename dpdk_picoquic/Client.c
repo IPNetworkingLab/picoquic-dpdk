215dc67b2c6d3ea5461a635ebfde51a6224ba2e0
Kikito <nikita.tyunyayev@gmail.com>
2021-11-09 15:18:34 +0100

    client/server working on ingi VM's

--- dpdk_picoquic/Client.c
+++ dpdk_picoquic/Client.c
@@ -36,7 +36,6 @@
 
 struct rte_mempool *mb_pool;
 
-
 static uint16_t nb_rxd = RTE_TEST_RX_DESC_DEFAULT;
 static uint16_t nb_txd = RTE_TEST_TX_DESC_DEFAULT;
 static struct rte_ether_addr eth_addr;
@@ -62,8 +61,6 @@ lcore_hello(__rte_unused void *arg)
 	struct rte_eth_conf local_port_conf = port_conf;
 	struct rte_ether_hdr *eth;
 	void *tmp;
-	
-	
 
 	tx_buffer = rte_zmalloc_socket("tx_buffer",
 								   RTE_ETH_TX_BUFFER_SIZE(MAX_PKT_BURST), 0,
@@ -143,33 +140,38 @@ lcore_hello(__rte_unused void *arg)
 	{
 		printf("failed to start device\n");
 	}
-	ret = rte_eth_promiscuous_enable(portid);
-	if (ret != 0)
-		rte_exit(EXIT_FAILURE,
-				 "rte_eth_promiscuous_enable:err=%s, port=%u\n",
-				 rte_strerror(-ret), portid);
+	// ret = rte_eth_promiscuous_enable(portid);
+	// if (ret != 0)
+	// 	rte_exit(EXIT_FAILURE,
+	// 			 "rte_eth_promiscuous_enable:err=%s, port=%u\n",
+	// 			 rte_strerror(-ret), portid);
 
 	while (true)
 	{
 		size_t pkt_size;
 		m = rte_pktmbuf_alloc(mb_pool);
 		if (m == NULL)
+		// printf("hello\n");
 		{
 			printf("fail to init pktmbuf\n");
 			return 0;
 		}
+		eth = rte_pktmbuf_mtod(m, struct rte_ether_hdr *);
+		// eth->ether_type = htons(2048);
+		rte_ether_addr_copy(&eth_addr, &eth->s_addr);
+		tmp = &eth->d_addr.addr_bytes[0];
+		*((uint64_t *)tmp) = 0;
+
 		pkt_size = sizeof(struct rte_ether_hdr);
-		m -> data_len = pkt_size;
-		m-> pkt_len = pkt_size;
-		
-		printf("size : %d\n", m->data_len);
+		m->data_len = pkt_size;
+		m->pkt_len = pkt_size;
 		ret = rte_eth_tx_buffer(0, 0, tx_buffer, m);
 		// ret = rte_eth_tx_burst(0, 0, &m,1);
 
-		if (ret != 0)
-		{
-			printf("send : %d\n", ret);
-		}
+		// if (ret != 0)
+		// {
+		// 	printf("send : %d\n", ret);
+		// }
 	}
 	return 0;
 }
@@ -183,11 +185,11 @@ int main(int argc, char **argv)
 	ret = rte_eal_init(argc, argv);
 	if (ret < 0)
 		rte_panic("Cannot init EAL\n");
-		
+
 	unsigned int nb_mbufs = RTE_MAX(1 * (1 + 1 + MAX_PKT_BURST + 2 * MEMPOOL_CACHE_SIZE), 8192U);
 	mb_pool = rte_pktmbuf_pool_create("mbuf_pool", nb_mbufs,
-														  MEMPOOL_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE,
-														  rte_socket_id());
+									  MEMPOOL_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE,
+									  rte_socket_id());
 	/* call lcore_hello() on every worker lcore */
 	// RTE_LCORE_FOREACH_WORKER(lcore_id)
 	// {
--- dpdk_picoquic/script_pktgen.sh
+++ dpdk_picoquic/script_pktgen.sh
@@ -1,1 +1,1 @@
-sudo /home/nikita/Documents/local/Pktgen-DPDK/usr/local/bin/pktgen -n 4 -- -P -p 0x1 -m "1.0" 
+sudo /root/remote/pktgen-dpdk/usr/local/bin/pktgen -n 4 -- -P -p 0x1 -m "1.0" 
