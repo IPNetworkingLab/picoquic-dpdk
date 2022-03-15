e52fde327645f722409083f568362248f016c70c
Kikito <nikita.tyunyayev@gmail.com>
2022-03-15 12:29:44 +0100

    didnt push everything

---
+++ dpdk_picoquicfirst/dpdk_picoquicdemo.h
@@ -0,0 +1,172 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef _WINDOWS
+#define WIN32_LEAN_AND_MEAN
+#include "getopt.h"
+#include <WinSock2.h>
+#include <Windows.h>
+
+#define SERVER_CERT_FILE "certs\\cert.pem"
+#define SERVER_KEY_FILE  "certs\\key.pem"
+
+#else /* Linux */
+
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+
+#ifndef __USE_XOPEN2K
+#define __USE_XOPEN2K
+#endif
+#ifndef __USE_POSIX
+#define __USE_POSIX
+#endif
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/select.h>
+#include <unistd.h>
+
+#define SERVER_CERT_FILE "certs/cert.pem"
+#define SERVER_KEY_FILE "certs/key.pem"
+
+#endif
+
+static const int default_server_port = 4443;
+static const char* default_server_name = "::";
+static const char* ticket_store_filename = "demo_ticket_store.bin";
+static const char* token_store_filename = "demo_token_store.bin";
+
+
+#include "picoquic.h"
+#include "picoquic_packet_loop.h"
+#include "picoquic_internal.h"
+#include "picoquic_utils.h"
+#include "autoqlog.h"
+#include "h3zero.h"
+#include "democlient.h"
+#include "demoserver.h"
+#include "siduck.h"
+#include "quicperf.h"
+#include "picoquic_unified_log.h"
+#include "picoquic_logger.h"
+#include "picoquic_binlog.h"
+#include "performance_log.h"
+#include "picoquic_config.h"
+#include "picoquic_lb.h"
+
+//dpdk
+#include <rte_common.h>
+#include <rte_log.h>
+#include <rte_malloc.h>
+#include <rte_memory.h>
+#include <rte_memcpy.h>
+#include <rte_eal.h>
+#include <rte_launch.h>
+#include <rte_atomic.h>
+#include <rte_cycles.h>
+#include <rte_prefetch.h>
+#include <rte_lcore.h>
+#include <rte_per_lcore.h>
+#include <rte_branch_prediction.h>
+#include <rte_interrupts.h>
+#include <rte_random.h>
+#include <rte_debug.h>
+#include <rte_ether.h>
+#include <rte_ethdev.h>
+#include <rte_mempool.h>
+#include <rte_mbuf.h>
+#include <rte_string_fns.h>
+#include <rte_ether.h>
+
+
+#define MAX_PKT_BURST 32
+#define MEMPOOL_CACHE_SIZE 256
+#define RTE_TEST_RX_DESC_DEFAULT 1024
+#define RTE_TEST_TX_DESC_DEFAULT 1024
+#define MAX_NB_OF_PORTS_AND_LCORES 32
+
+#define PICOQUIC_SAMPLE_ALPN "picoquic_sample"
+#define PICOQUIC_SAMPLE_SNI "test.example.com"
+
+#define PICOQUIC_SAMPLE_NO_ERROR 0
+#define PICOQUIC_SAMPLE_INTERNAL_ERROR 0x101
+#define PICOQUIC_SAMPLE_NAME_TOO_LONG_ERROR 0x102
+#define PICOQUIC_SAMPLE_NO_SUCH_FILE_ERROR 0x103
+#define PICOQUIC_SAMPLE_FILE_READ_ERROR 0x104
+#define PICOQUIC_SAMPLE_FILE_CANCEL_ERROR 0x105
+
+#define PICOQUIC_SAMPLE_CLIENT_TICKET_STORE "sample_ticket_store.bin";
+#define PICOQUIC_SAMPLE_CLIENT_TOKEN_STORE "sample_token_store.bin";
+#define PICOQUIC_SAMPLE_CLIENT_QLOG_DIR ".";
+#define PICOQUIC_SAMPLE_SERVER_QLOG_DIR ".";
+
+//client
+
+typedef struct st_client_loop_cb_t {
+    picoquic_cnx_t* cnx_client;
+    picoquic_demo_callback_ctx_t* demo_callback_ctx;
+    siduck_ctx_t* siduck_ctx;
+    int notified_ready;
+    int established;
+    int migration_to_preferred_started;
+    int migration_to_preferred_finished;
+    int migration_started;
+    int address_updated;
+    int force_migration;
+    int nb_packets_before_key_update;
+    int key_update_done;
+    int zero_rtt_available;
+    int is_siduck;
+    int is_quicperf;
+    int socket_buffer_size;
+    int handshake_test;
+    char const* saved_alpn;
+    struct sockaddr_storage server_address;
+    struct sockaddr_storage client_address;
+    picoquic_connection_id_t server_cid_before_migration;
+    picoquic_connection_id_t client_cid_before_migration;
+} client_loop_cb_t;
+
+static const char * test_scenario_default = "0:index.html;4:test.html;8:/1234567;12:main.jpg;16:war-and-peace.txt;20:en/latest/;24:/file-123K";
+
+int client_loop_cb(picoquic_quic_t* quic, picoquic_packet_loop_cb_enum cb_mode, 
+    void* callback_ctx, void * callback_arg);
+
+int quic_client(const char* ip_address_text, int server_port, 
+    picoquic_quic_config_t * config, int force_migration,
+    int nb_packets_before_key_update, char const * client_scenario_text, int handshake_test, int dpdk, unsigned portid,
+    unsigned queueid,
+                           struct sockaddr_storage *addr_from,
+                           struct rte_ether_addr *mac_dst,
+                           struct rte_mempool *mb_pool,
+                           struct rte_eth_dev_tx_buffer *tx_buffer);
+            
+
+//server
+
+typedef struct st_server_loop_cb_t {
+    int just_once;
+    int first_connection_seen;
+    int connection_done;
+} server_loop_cb_t;
+
+static int server_loop_cb(picoquic_quic_t* quic, picoquic_packet_loop_cb_enum cb_mode,
+    void* callback_ctx, void * callback_arg);
+
+int quic_server(const char* server_name, 
+                        picoquic_quic_config_t * config, 
+                        int just_once, 
+                        int dpdk,
+                        unsigned portid,
+                        unsigned queueid,
+                        struct sockaddr_storage *addr_from,
+                        struct rte_ether_addr *mac_dst,
+                        struct rte_mempool *mb_pool,
+                        struct rte_eth_dev_tx_buffer *tx_buffer);
+
+
---
+++ dpdk_picoquicfirst/dpdk_server.c
@@ -0,0 +1,143 @@
+#include "dpdk_picoquicdemo.h"
+
+static int server_loop_cb(picoquic_quic_t* quic, picoquic_packet_loop_cb_enum cb_mode,
+    void* callback_ctx, void * callback_arg)
+{
+    int ret = 0;
+    server_loop_cb_t* cb_ctx = (server_loop_cb_t*)callback_ctx;
+
+    if (cb_ctx == NULL) {
+        ret = PICOQUIC_ERROR_UNEXPECTED_ERROR;
+    }
+    else {
+        switch (cb_mode) {
+        case picoquic_packet_loop_ready:
+            fprintf(stdout, "Waiting for packets.\n");
+            break;
+        case picoquic_packet_loop_after_receive:
+            break;
+        case picoquic_packet_loop_after_send:
+            break;
+        case picoquic_packet_loop_port_update:
+            break;
+        default:
+            ret = PICOQUIC_ERROR_UNEXPECTED_ERROR;
+            printf("error\n");
+            break;
+        }
+
+        if (ret == 0 && cb_ctx->just_once){
+            if (!cb_ctx->first_connection_seen && picoquic_get_first_cnx(quic) != NULL) {
+                cb_ctx->first_connection_seen = 1;
+                fprintf(stdout, "First connection noticed.\n");
+            } else if (cb_ctx->first_connection_seen && picoquic_get_first_cnx(quic) == NULL) {
+                fprintf(stdout, "No more active connections.\n");
+                cb_ctx->connection_done = 1;
+                ret = PICOQUIC_NO_ERROR_TERMINATE_PACKET_LOOP;
+            }
+        }
+    }
+    return ret;
+}
+
+int quic_server(const char* server_name, 
+                        picoquic_quic_config_t * config, 
+                        int just_once,
+                        int dpdk, 
+                        unsigned portid,
+                        unsigned queueid,
+                        struct sockaddr_storage *addr_from,
+                        struct rte_ether_addr *mac_dst,
+                        struct rte_mempool *mb_pool,
+                        struct rte_eth_dev_tx_buffer *tx_buffer)
+{
+    /* Start: start the QUIC process with cert and key files */
+    int ret = 0;
+    picoquic_quic_t* qserver = NULL;
+    uint64_t current_time = 0;
+    picohttp_server_parameters_t picoquic_file_param;
+    server_loop_cb_t loop_cb_ctx;
+
+    memset(&picoquic_file_param, 0, sizeof(picohttp_server_parameters_t));
+    picoquic_file_param.web_folder = config->www_dir;
+    memset(&loop_cb_ctx, 0, sizeof(server_loop_cb_t));
+    loop_cb_ctx.just_once = just_once;
+
+    /* Setup the server context */
+    if (ret == 0) {
+        current_time = picoquic_current_time();
+        /* Create QUIC context */
+
+        if (config->ticket_file_name == NULL) {
+            ret = picoquic_config_set_option(config, picoquic_option_Ticket_File_Name, ticket_store_filename);
+        }
+        if (ret == 0 && config->token_file_name == NULL) {
+            ret = picoquic_config_set_option(config, picoquic_option_Token_File_Name, token_store_filename);
+        }
+        if (ret == 0) {
+            qserver = picoquic_create_and_configure(config, picoquic_demo_server_callback, &picoquic_file_param, current_time, NULL);
+            if (qserver == NULL) {
+                ret = -1;
+            }
+            else {
+                picoquic_set_key_log_file_from_env(qserver);
+
+                picoquic_set_alpn_select_fn(qserver, picoquic_demo_server_callback_select_alpn);
+
+                picoquic_set_mtu_max(qserver, config->mtu_max);
+                if (config->qlog_dir != NULL)
+                {
+                    picoquic_set_qlog(qserver, config->qlog_dir);
+                }
+                if (config->performance_log != NULL)
+                {
+                    ret = picoquic_perflog_setup(qserver, config->performance_log);
+                }
+                if (ret == 0 && config->cnx_id_cbdata != NULL) {
+                    picoquic_load_balancer_config_t lb_config;
+                    ret = picoquic_lb_compat_cid_config_parse(&lb_config, config->cnx_id_cbdata, strlen(config->cnx_id_cbdata));
+                    if (ret != 0) {
+                        fprintf(stdout, "Cannot parse the CNX_ID config policy: %s.\n", config->cnx_id_cbdata);
+                    }
+                    else {
+                        ret = picoquic_lb_compat_cid_config(qserver, &lb_config);
+                        if (ret != 0) {
+                            fprintf(stdout, "Cannot set the CNX_ID config policy: %s.\n", config->cnx_id_cbdata);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    if (ret == 0) {
+        /* Wait for packets */
+#if _WINDOWS
+        ret = picoquic_packet_loop_win(qserver, config->server_port, 0, config->dest_if, 
+            config->socket_buffer_size, server_loop_cb, &loop_cb_ctx);
+#else
+        if(dpdk){
+            ret = picoquic_packet_loop_dpdk(qserver, config->server_port, 0, config->dest_if,
+            config->socket_buffer_size, config->do_not_use_gso, server_loop_cb, &loop_cb_ctx, portid,queueid, *addr_from,NULL,mb_pool, tx_buffer);
+        }
+        else{
+            ret = picoquic_packet_loop(qserver, config->server_port, 0, config->dest_if,
+            config->socket_buffer_size, config->do_not_use_gso, server_loop_cb, &loop_cb_ctx);
+        }
+        
+#endif
+    }
+
+    /* And exit */
+    printf("Server exit, ret = 0x%x\n", ret);
+
+    /* Clean up */
+    if (config->cnx_id_cbdata != NULL) {
+        picoquic_lb_compat_cid_config_free(qserver);
+    }
+    if (qserver != NULL) {
+        picoquic_free(qserver);
+    }
+
+    return ret;
+}
