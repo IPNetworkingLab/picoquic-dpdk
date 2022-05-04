3326eb650a6481947ca40c744ee56f4e8b1ebc4f
kikito <nikita.tyunyayev@student.uclouvain.be>
2022-05-04 17:12:13 +0000

    adapted mac addresses to new setup

--- exec_scripts/serverProxy.sh
+++ exec_scripts/serverProxy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo ./dpdk_picoquicdemo dpdk -l 4-5 -a 0000:8a:01.5 -a 0000:8a:01.6 --proc-type=primary --file-prefix=rte_2 --socket-mem=2000 -- -2 4e:ee:af:42:01:e5 -a proxy -p 4443 
+sudo ./dpdk_picoquicdemo dpdk -l 4-5 -a 0000:8a:02.5 -a 0000:8a:02.6 --proc-type=primary --file-prefix=rte_2 --socket-mem=2000 -- -2 4e:ee:af:42:01:e5 -a proxy -p 4443 
